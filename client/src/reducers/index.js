e156ecc05908d6a43d9621df63ea5f6089f96763
gparkkii <jyp933@gmail.com>
2021-04-18 03:50:03 +0900

    Add :arrow_right: Post 리듀서 생성

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 
 import user from './user';
+import post from './post';
 
 const rootReducer = combineReducers({
   index: (state = {}, action) => {
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
     }
   },
   user,
+  post,
 });
 
 export default rootReducer;
---
+++ client/src/reducers/post.js
@@ -0,0 +1,76 @@
+export const initialState = {
+  mainPosts: [
+    {
+      id: 0,
+      User: {
+        id: 0,
+        nickname: '',
+      },
+      content: '',
+      Images: [],
+      Comments: [
+        {
+          User: {
+            nickname: '',
+          },
+          content: '',
+        },
+        {
+          User: {
+            nickname: '',
+          },
+          content: '',
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = 'ADD_POST';
+
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 1,
+  User: {
+    id: 1,
+    nickname: '박지연',
+  },
+  content: '계속 성장하는 개발자 박지연입니다.',
+  Images: [],
+  Comments: [
+    {
+      User: {
+        nickname: '보노보노',
+      },
+      content: '힘내세요~',
+    },
+    {
+      User: {
+        nickname: '도라에몽',
+      },
+      content: '화이팅~',
+    },
+  ],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST: {
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    }
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
