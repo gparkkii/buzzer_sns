c376e00b68f785ec292049309dc85554eae19891
gparkkii <jyp933@gmail.com>
2021-04-17 19:24:47 +0900

    Fix :arrow_right: eslint 설정 수정

---
+++ client/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'prettier'],
+  ignorePatterns: ['node_modules/'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'arrow-parens': ['off', 'always'],
+    'jsx-a11y/label-has-associated-control': ['off', 'always'],
+    'jsx-a11y/anchor-is-valid': ['off', 'always'],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+};
